7bb2cd54cf746ce08d35b08fc7c11abc250adff7
Antonio <atejda8071@southwestuniversity.edu>
2021-05-30 00:34:12 -0600

    Fix and Added List

---
+++ CommentD.cs
@@ -0,0 +1,18 @@
+﻿using System;
+namespace MyInstaGrem1._2
+{
+    public class CommentData
+    {
+        public string Comment { get; }
+
+        public string Data;
+        public string userID;
+        public string getdata()
+
+        // public string CommentData { get; set; }//
+        {
+            return this.Data + this.userID;
+        }
+
+    }
+}
--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System;
+using System.Collections.Generic;
 
 namespace MyInstaGrem1._2
 {
@@ -6,6 +7,7 @@ namespace MyInstaGrem1._2
 
     class Program
     {
+
 
         static void Main(string[] args)
         {
@@ -30,7 +32,7 @@ namespace MyInstaGrem1._2
 
         {
             Console.WriteLine("Select 1: To Post On Your Profile ");
-            Console.WriteLine("Select 2: To View Reply Comments ");
+            Console.WriteLine("Select 2: To View Users Comments ");
             Console.WriteLine("Select 3: To Search Users ");
             Console.WriteLine("Select 4: To Exit ");
             string mySelection;
@@ -58,8 +60,9 @@ namespace MyInstaGrem1._2
 
         static void Post()
         {
-            Console.ForegroundColor = ConsoleColor.DarkBlue;
+            Console.ForegroundColor = ConsoleColor.Blue;
             Console.WriteLine("Write Your Post!");
+            string Cpost = Console.ReadLine();
             Console.ReadLine();
 
 
@@ -68,10 +71,36 @@ namespace MyInstaGrem1._2
         static void Comments()
         {
             Console.ForegroundColor = ConsoleColor.Green;
-            Console.WriteLine("View Your Comments!");
+            Console.WriteLine("Type Something To View Your Comments!");
+            string CommentD = Console.ReadLine();
+            Console.WriteLine("Your Comment: " + CommentD);
+     
+            List<CommentData> commentlist = new List<CommentData>();
+            commentlist.Add(new CommentData());
+            commentlist[0].Data = "~~ " + "Wolfz you wanna play some rainbow six? ";
+            commentlist[0].userID = ":"+ " mohammed";
+            
+
+            commentlist.Add(new CommentData());
+            commentlist[1].userID = ":"+ " wolfz";
+            commentlist[1].Data = "~~ " + "Hello XD mohammed! send me a invite ";
+
+            commentlist.Add(new CommentData());
+            commentlist[2].Data = "~~ " + "a fresh slushie with this hot weather ";
+            commentlist[2].userID = ":" + " don";
+
+            commentlist.Add(new CommentData());
+            commentlist[3].Data = "~~ " + "any fun movies to watch? ";
+            commentlist[3].userID = ":" + " maria";
+
+            commentlist.Add(new CommentData());
+            commentlist[4].Data = "~~ " + "testing my new compressor ";
+            commentlist[4].userID = ":" + " Goofy";
+            foreach (CommentData Calls in commentlist)
+            {
+                Console.WriteLine(Calls.getdata());
+            }
             Console.ReadLine();
-
-
 
         }
         static void Users()
@@ -89,7 +118,7 @@ namespace MyInstaGrem1._2
             Console.WriteLine("Are You Sure You Want to Quit? ");
             Console.WriteLine("Type Enter To Confirm!");
             Console.ReadLine();
-            System.Environment.Exit(1);
+            Environment.Exit(1);
 
 
         }
